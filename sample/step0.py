8906ba79893e79e337335b7ad834a29bd0f1673a
impurity <impurity@postech.ac.kr>
2016-09-30 15:20:36 +0900

    initial

--- sample/step0.py
+++ sample/step0.py
@@ -1,6 +1,7 @@
 
 from ase import Atoms, Atom
 from ase.calculators.lammpsrun import LAMMPS
+from ase.visualize import view
 
 a = [6.5, 6.5, 7.7]
 d = 2.3608
@@ -9,4 +10,8 @@ NaCl = Atoms([Atom('Na', [0,0,0]), Atom('Cl', [0,0,d])], cell=a, pbc=True)
 calc = LAMMPS()
 NaCl.set_calculator(calc)
 
-print(NaCl.get_stress())
+view(NaCl)
+
+# print NaCl.get_total_energy()
+
+# print(NaCl.get_stress())
